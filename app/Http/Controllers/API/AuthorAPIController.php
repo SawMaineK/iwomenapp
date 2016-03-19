7b5c23c2f583a6be54dcea02ad1be46cc226f063
SMK <sawmainek90@gmail.com>
2016-03-19 11:25:38 +0630

    update

--- app/Http/Controllers/API/AuthorAPIController.php
+++ app/Http/Controllers/API/AuthorAPIController.php
@@ -6,6 +6,7 @@ use App\Models\Author;
 use Illuminate\Http\Request;
 use App\Http\Controllers\AppBaseController as AppBaseController;
 use Response;
+use Validator;
 
 class AuthorAPIController extends AppBaseController
 {
