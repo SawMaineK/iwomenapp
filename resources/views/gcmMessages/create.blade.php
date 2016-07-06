8ad9699d1d936e87c52545ae62a15090ad221a6b
SMK <sawmainek90@gmail.com>
2016-07-06 17:42:08 +0630

    update

--- resources/views/gcmMessages/create.blade.php
+++ resources/views/gcmMessages/create.blade.php
@@ -3,12 +3,6 @@
 @section('content')
 <div class="container">
 
-    @include('common.errors')
-
-    {!! Form::open(['route' => 'gcmMessages.store', 'enctype'=>'multipart/form-data']) !!}
-
-        @include('gcmMessages.fields')
-
-    {!! Form::close() !!}
+    
 </div>
 @endsection
--- resources/views/gcmMessages/fields.blade.php
+++ resources/views/gcmMessages/fields.blade.php
@@ -10,6 +10,25 @@
 	{!! Form::text('message', null, ['class' => 'form-control']) !!}
 </div>
 
+<!-- User Id Field -->
+<div class="form-group col-sm-6 col-lg-4">
+    {!! Form::label('user_id', 'User Id:') !!}
+    <select name="user_id" class="form-control">
+	    <option value="All">All</option>
+	   	@foreach($users as $row)
+	   		@if(isset($row['username']))
+	    	<option value="{!! $row->objectId !!}">{!!$row['username']!!}</option>
+	    	@endif
+	    @endforeach
+    </select>
+</div>
+
+<!-- Image Field -->
+<div class="form-group col-sm-6 col-lg-4">
+    {!! Form::label('image', 'Image:') !!}
+	{!! Form::file('image') !!}
+</div>
+
 
 <!-- Submit Field -->
 <div class="form-group col-sm-12">
